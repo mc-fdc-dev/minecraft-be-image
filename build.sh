087c4c62a3166767485ac58a88cb2f31224d409a
mc_fdc <Masato.04.11.2007@gmail.com>
2023-03-23 05:46:50 +0000

    bump version

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-curl https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.51.01.zip > bedrock-server.zip
+curl https://minecraft.azureedge.net/bin-linux/bedrock-server-1.19.72.01.zip > bedrock-server.zip
 unzip bedrock-server.zip
