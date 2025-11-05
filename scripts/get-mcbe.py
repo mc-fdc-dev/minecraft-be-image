8c7a135176ba55778fa54debf7cca442b505998c
tuna2134 <contact@tuna2134.dev>
2025-11-05 09:42:55 +0900

    Update User-Agent for requests in get-mcbe.py

--- scripts/get-mcbe.py
+++ scripts/get-mcbe.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 r = requests.get("https://www.minecraft.net/ja-jp/download/server/bedrock", headers={
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
 })
 soup = BeautifulSoup(r.text, "html.parser")
